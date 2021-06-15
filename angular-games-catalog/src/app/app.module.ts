5a2d4a05b157ce015a59c07c8f522cabd1ca05bf
Dmitry Veko <vekodmitriy0@gmail.com>
2021-06-15 12:25:22 +0300

    feat: create signInUp component

--- angular-games-catalog/src/app/app.module.ts
+++ angular-games-catalog/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInUpComponent
   ],
   imports: [
     BrowserModule,
