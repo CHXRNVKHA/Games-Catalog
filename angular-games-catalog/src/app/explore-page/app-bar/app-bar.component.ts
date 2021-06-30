53c4d78c7859e5c92c9536ad638fd89a085022d3
Dzmitry Veka <Dzmitry.Veka@itechart-group.com>
2021-06-30 18:04:29 +0300

    feat: add logout

--- angular-games-catalog/src/app/explore-page/app-bar/app-bar.component.ts
+++ angular-games-catalog/src/app/explore-page/app-bar/app-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../services/authentication.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-app-bar',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-bar.component.css']
 })
 export class AppBarComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
