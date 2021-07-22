9172862a27c96087f16227145728dd64730fcd91
Dmitriy Veko <vekodmitriy0@gmail.com>
2021-07-22 20:38:13 +0300

    refactor: change file location

--- angular-games-catalog/src/app/services/game.service.ts
+++ angular-games-catalog/src/app/services/game.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-import { Game } from '../game';
+import { Game } from '../models/game';
 import { environment } from 'src/environments/environment';
 import { HttpParams } from "@angular/common/http";
 
