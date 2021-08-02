b10e7747cd6f92ebcdc7f634bcadb42257e8551f
Dzmitry Veka <Dzmitry.Veka@itechart-group.com>
2021-08-02 13:14:24 +0300

    feat: change names of games

--- angular-games-catalog/src/app/in-memory-data.service.ts
+++ angular-games-catalog/src/app/in-memory-data.service.ts
@@ -9,13 +9,13 @@ export class InMemoryDataService {
   createDb() {
     const games = [
       { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted1',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted2',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted3',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted4',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted5',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted6',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 11, name: 'Uncharted7',  date:'2016 May 10', rating: 94.4, desctiption: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
     ];
     return {games};
   }
