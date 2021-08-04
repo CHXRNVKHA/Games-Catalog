3aa6ee3298db11995dce7ccd8529e91ed471593d
Dzmitry Veka <Dzmitry.Veka@itechart-group.com>
2021-08-04 15:17:50 +0300

    fix: change ID's for games in data array

--- angular-games-catalog/src/app/services/in-memory-data.service.ts
+++ angular-games-catalog/src/app/services/in-memory-data.service.ts
@@ -8,14 +8,14 @@ import { Game } from '../models/game';
 export class InMemoryDataService {
   public createDb() {
     const games = [
-      { id: 11, name: 'Uncharted ',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted1',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted2',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted3',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted4',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted5',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted6',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
-      { id: 11, name: 'Uncharted7',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 1, name: 'Uncharted ',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 2, name: 'Uncharted1',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 3, name: 'Uncharted2',  date:'2016 May 10', raiting: 94.4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend arcu in magna porttitor egestas ut et elit.', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 4, name: 'Uncharted3',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 5, name: 'Uncharted4',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 6, name: 'Uncharted5',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 7, name: 'Uncharted6',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
+      { id: 8, name: 'Uncharted7',  date:'2016 May 10', raiting: 94.4, description: 'desc', imageUrl: '.../../../assets/images/Uncharted_4_cover.jpg'},
     ];
     const users = [
       { id: 1, userName: 'testUser1', email: 'test@gmail1.com', password: 'test' },
